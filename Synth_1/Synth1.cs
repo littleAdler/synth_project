5e755b580872fc5a036b647394e499e481975cd3
Mike <mspouge@gmail.com>
2021-02-20 23:02:31 -0800

    converted everything to bytes to make the data stream loading easier

--- Synth_1/Synth1.cs
+++ Synth_1/Synth1.cs
@@ -29,7 +29,7 @@ namespace Synth_1
         }
         private void Synth1_KeyDown(object sender, EventArgs e) { 
         }
-        private void button1_Click(object sender, EventArgs e)
+        private void Button1_Click(object sender, EventArgs e)
         {
             //define wave storage over 1 second
             short[] wave = new short[SAMPLE_RATE];
@@ -49,58 +49,48 @@ namespace Synth_1
             //The canonical WAVE format starts with the RIFF header:
 
                 /*Offset*/
-                /*0  */ char[] ChunkID = new char[4] { 'R', 'I', 'F', 'F' };
-                /*(22) */  short NumChannels = 1;
-                /*(32) */ short blockAlign = BITS_PER_SAMPLE / 8;
-                
-                /*(40) */ int Subchunk2Size = SAMPLE_RATE * NumChannels * blockAlign;
-                /*4  */ int ChunkSize = 36 + Subchunk2Size;
-                /*8  */ char[] Format = new char[4] { 'W', 'A', 'V', 'E' };
+
+                /*0  */ byte[] ChunkID = new byte[4] { (byte)'R', (byte)'I', (byte)'F', (byte)'F' };
+                /*(22) */  byte[] NumChannels = BitConverter.GetBytes((short)1);
+                /*(32) */ byte[] blockAlign = BitConverter.GetBytes((short)(BITS_PER_SAMPLE / 8));
+                /*(40) */ byte[] Subchunk2Size = BitConverter.GetBytes((int)(SAMPLE_RATE * BitConverter.ToInt16(NumChannels,0) * BitConverter.ToInt16(blockAlign,0)));
+                /*4  */ byte[] ChunkSize = BitConverter.GetBytes((int)(36 + BitConverter.ToInt32(Subchunk2Size,0)));
+                /*8  */ byte[] Format = new byte[4] { (byte)'W', (byte)'A', (byte)'V', (byte)'E' };
 
             //The "WAVE" format consists of two subchunks: "fmt " and "data":
 
             //~~~~~~~~ FMT Subchunk ~~~~~~~~\\
 
             //The "fmt " subchunk describes the sound data's format:
-
-                /*12 */ char[] Subchunk1ID = new char[4] {'f','m','t',' '};
-                /*16 */ int Subchunk1Size = 16;
-                /*20 */ short AudioFormat = 1;
+                
+                /*12 */ byte[] Subchunk1ID = new byte[4] {(byte)'f',(byte)'m',(byte)'t',(byte)' '};
+                /*16 */ byte[] Subchunk1Size = BitConverter.GetBytes((int)16);
+                /*20 */ byte[] AudioFormat = BitConverter.GetBytes((short)1);
                 /*22 */ //NumChannels
-                /*24 */ int SampleRate = SAMPLE_RATE;
-                /*28 */ int ByteRate = SampleRate*NumChannels*blockAlign;
+                /*24 */ byte[] SampleRate = BitConverter.GetBytes((int)SAMPLE_RATE);
+                /*28 */ byte[] ByteRate = BitConverter.GetBytes((int)(BitConverter.ToInt32(SampleRate,0)* BitConverter.ToInt16(NumChannels, 0) * BitConverter.ToInt16(blockAlign, 0)));
                 /*32 */ //Block Align
-                /*34 */ short BitsPerSample = BITS_PER_SAMPLE;
+                /*34 */ byte[] BitsPerSample = BitConverter.GetBytes((short)BITS_PER_SAMPLE);
            
             //~~~~~~~~ DATA Subchunk ~~~~~~~~\\
 
             //The "data" subchunk contains the size of the data and the actual sound:
 
-                /*36 */char[] Subchunk2ID = new char[4] {'d','a','t','a'};
+                /*36 */byte[] Subchunk2ID = new byte[4] {(byte)'d',(byte)'a',(byte)'t',(byte)'a'};
                 /*40 */ //Subchunk2Size
                 /*44 */ byte[] Data = binarywave;
 
+                //Combine the wave formate parameters into a single array to be written to the Memory Stream via the Binary Writer
+                byte[] wav = ChunkID.Concat(ChunkSize).Concat(Format).Concat(Subchunk1ID).Concat(Subchunk1Size).Concat(AudioFormat).Concat(NumChannels).Concat(SampleRate)
+                                    .Concat(ByteRate).Concat(blockAlign).Concat(BitsPerSample).Concat(Subchunk2ID).Concat(Subchunk2Size).Concat(Data).ToArray();
 
 
 
-            byte[] bufferRead = new byte[43];
+            //byte[] bufferRead = new byte[43];
             //---------------------------Write the .wav pararmeters through the memory stream and play them ---------------------------\\
             using (MemoryStream memoryStream = new MemoryStream())
             using (BinaryWriter binaryWriter = new BinaryWriter(memoryStream)) {
-                binaryWriter.Write(ChunkID);
-                binaryWriter.Write(ChunkSize);
-                binaryWriter.Write(Format);
-                binaryWriter.Write(Subchunk1ID);
-                binaryWriter.Write(Subchunk1Size);
-                binaryWriter.Write(AudioFormat);
-                binaryWriter.Write(NumChannels);
-                binaryWriter.Write(SampleRate);
-                binaryWriter.Write(ByteRate);
-                binaryWriter.Write(blockAlign);
-                binaryWriter.Write(BitsPerSample);
-                binaryWriter.Write(Subchunk2ID);
-                binaryWriter.Write(Subchunk2Size);
-                binaryWriter.Write(Data);
+                binaryWriter.Write(wav);
                 memoryStream.Position = 0;
                 new SoundPlayer(memoryStream).Play();
 
@@ -108,10 +98,5 @@ namespace Synth_1
 
 
         }
-
-        /*private void button1_Click(object sender, EventArgs e)
-        {
-
-        }*/
     }
 }
